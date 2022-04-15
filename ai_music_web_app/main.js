d4a59bae7612c2e20b4f3e7ab917ae7adb5e06f6
devan2009 <78412423+devan2009@users.noreply.github.com>
2022-04-15 17:44:01 +0530

    Add files via upload

--- ai_music_web_app/main.js
+++ ai_music_web_app/main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -9,6 +13,8 @@ function preload()
 function play()
 {
     song1.play();
+    song.setvoulume(1);
+    song.rate(1);
 }
 function setup()
 {
@@ -17,6 +23,28 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+function modelLoaded()
+{
+    console.log('PoseNet Is Inistalized');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+    }
 }
 function draw()
 {
